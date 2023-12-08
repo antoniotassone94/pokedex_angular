461c4f30e3b58b0799e7362bec7b48eeb5389851
antoniotassone94 <antonio.tassone@studio.unibo.it>
2023-12-08 07:29:30 +0100

    added localstorage to memorize actual pokedex in memory. After each insertion the localstorage is updated.

--- src/app/components/pokedex/pokedex.component.ts
+++ src/app/components/pokedex/pokedex.component.ts
@@ -31,6 +31,26 @@ export class PokedexComponent implements OnInit{
   }
 
   public ngOnInit():void{
+    if(localStorage.getItem("pokedex") !== null){
+      const pokedexEsistente:any[] = JSON.parse(<string>localStorage.getItem("pokedex"));
+      for(let i = 0;i < pokedexEsistente.length;i++){
+        const pokemonAttuale:PokemonModel = new PokemonModel();
+        pokemonAttuale.name = pokedexEsistente[i]._name;
+        pokemonAttuale.height = pokedexEsistente[i]._height;
+        pokemonAttuale.weight = pokedexEsistente[i]._weight;
+        pokemonAttuale.hp = pokedexEsistente[i]._hp;
+        pokemonAttuale.attack = pokedexEsistente[i]._attack;
+        pokemonAttuale.defense = pokedexEsistente[i]._defense;
+        pokemonAttuale.specialAttack = pokedexEsistente[i]._specialAttack;
+        pokemonAttuale.specialDefense = pokedexEsistente[i]._specialDefense;
+        pokemonAttuale.speed = pokedexEsistente[i]._speed;
+        pokemonAttuale.backDefault = pokedexEsistente[i]._backDefault;
+        pokemonAttuale.backShiny = pokedexEsistente[i]._backShiny;
+        pokemonAttuale.frontDefault = pokedexEsistente[i]._frontDefault;
+        pokemonAttuale.frontShiny = pokedexEsistente[i]._frontShiny;
+        this._pokemonList.push(pokemonAttuale);
+      }
+    }
     this.insertinpokedex.getNewPokemon().subscribe({
       next: (newpokemon:PokemonModel|undefined) => {
         if(newpokemon){
@@ -41,6 +61,7 @@ export class PokedexComponent implements OnInit{
           if(i >= this._pokemonList.length){
             if(this._pokemonList.length < 10){
               this._pokemonList.push(newpokemon);
+              localStorage.setItem("pokedex",JSON.stringify(this._pokemonList));
               this._message = "";
             }else{
               this._message = "Il pokedex è pieno!";
